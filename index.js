27e3b6ae897a89ff060e19d9f362e4bb3a1d50bf
Edwin Mwaniki <emwaniki91@gmail.com>
2022-01-14 21:55:50 +0300

    Finish Basic CRUD

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const path = require('path');
 const mongoose = require('mongoose');
 const Campground = require('./models/campground');
+const methodOverride = require('method-override')
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp')
 
@@ -18,23 +19,55 @@ mongoose.connect('mongodb://localhost:27017/yelp-camp')
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
+
 app.get('/', (req, res) => {
     res.render('home')
-})
+});
+
 app.get('/campgrounds', async (req, res) => {
     const camps = await Campground.find({})
     res.render('campgrounds/index', { camps })
+});
+
+app.get('/campgrounds/new', (req, res) => {
+    res.render('campgrounds/new')
+})
+
+app.post('/campgrounds', async (req, res) => {
+    const campground = new Campground(req.body.campgrounds)
+    console.log(campground);
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`)
 })
 
 app.get('/campgrounds/:id', async (req, res) => {
     const { id } = req.params;
     const foundCamp = await Campground.findById(id);
     res.render('campgrounds/show', { foundCamp })
+});
 
+app.get('/campgrounds/:id/edit', async (req, res) => {
+    const { id } = req.params;
+    const foundCamp = await Campground.findById(id);
+    res.render('campgrounds/edit', { foundCamp })
+});
 
+app.put('/campgrounds/:id', async (req, res) => {
+    const { id } = req.params;
+    // res.send(req.body)
+    const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
+    console.log(campground);
+    res.redirect(`/campgrounds/${campground._id}`)
+});
+
+app.delete('/campgrounds/:id', async (req, res) => {
+    const { id } = req.params;
+    await Campground.findByIdAndDelete(id);
+    res.redirect('/campgrounds')
 })
-
 
 app.listen(3000, () => {
     console.log('APP IS LISTENING AT PORT 3000');
-})
+});
